8c44368edc6b1fd571b165e340eb359acb01e99e
Miwka <97275239+albertu4@users.noreply.github.com>
2022-02-18 22:29:11 +0900

    Added enum in weather model

---
+++ WeatherApp/Models/Weather.swift
@@ -0,0 +1,13 @@
+//
+//  Weather.swift
+//  WeatherApp
+//
+//  Created by Михаил Иванов on 18.02.2022.
+
+enum Link: String {
+    case londonWeather = "https://weatherdbi.herokuapp.com/data/weather/london"
+    case moscowWeather = "https://weatherdbi.herokuapp.com/data/weather/moscow"
+    case newYorkWeather = "https://weatherdbi.herokuapp.com/data/weather/newYork"
+    case yakutskWeather = "https://weatherdbi.herokuapp.com/data/weather/yakutsk"
+    case seoulWeather = "https://weatherdbi.herokuapp.com/data/weather/seoul"
+}
---
+++ WeatherApp/Services/NetworkManager.swift
@@ -0,0 +1,7 @@
+//
+//  NetworkManager.swift
+//  WeatherApp
+//
+//  Created by Михаил Иванов on 18.02.2022.
+//
+
